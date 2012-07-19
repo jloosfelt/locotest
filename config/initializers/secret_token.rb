a18a6a1459c4f199684abfcf6cb51cc3f51a71f7
Jérémie Loosfelt <jloosfelt@gmail.com>
2012-07-19 14:02:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Locotest::Application.config.secret_token = '07141923a0dd151523e0188117238dc29f778f93d401e0fdd926cf00ec881e9d550200009e4bbafd1f01ba96d2c95c6d570410c06b1cc42f60b031ef0aee501d'
